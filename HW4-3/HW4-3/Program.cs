2cd438179fcc3728f952b9c92b6d3b74b3a92b1b
LiuAmos <b10332011@gmail.com>
2018-10-15 21:10:40 +0800

    20181015lab

---
+++ HW4-3/HW4-3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW4_3
+{
+    delegate int Op(int d1, int d2);
+    
+    class Program
+    {
+        public static int Mul(int m1, int m2)
+        {
+            return m1*m2;
+        }
+
+        public static int Plus(int p1, int p2)
+        {
+            return p1+p2;
+        }
+
+        public static int Less(int l1, int l2)
+        {
+            return l1-l2;
+        }
+
+        public static int Result(int x, int y, Op tv)
+        {
+            int re = tv(x, y);
+            return re;
+        }
+
+        static void Main(string[] args)
+        {
+            int ten = 10, three = 3,two=2,five=5,temp,temp1,temp2;
+            Console.WriteLine("{0}+{1}*{2}-{3}\n", ten, three, two, five);
+
+            Op o;
+            o = new Op(Mul);
+            temp = Result(three, two, o);
+            Console.WriteLine("{0}+{1}-{2}\n",ten,temp,five);
+
+            Op o1;
+            o1 = new Op(Plus);
+            temp1 = Result(ten,temp, o1);
+            Console.WriteLine("{0}-{1}\n",temp1, five);
+
+            Op o2;
+            o2 = new Op(Less);
+            temp2 = Result(temp1,five, o2);
+            Console.WriteLine("{0}\n", temp2);
+
+            Console.Read();
+        }
+    }
+}
---
+++ hw4-2/hw4-2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hw4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string num;
+            double l,w;
+            while (true)
+            {
+                Console.Write("請選擇形狀（1.三角形 2.矩形 3.菱形 4.平行四邊形 0.離開）\n");
+                num = Console.ReadLine();
+
+                if (num == "0")
+                {
+                    break;
+                }
+
+                switch (num)
+                {
+                    case "1":
+                        Console.Write("請輸入 高  ");
+                        l= double.Parse(Console.ReadLine());
+                        Console.Write("請輸入 底  ");
+                        w= double.Parse(Console.ReadLine());
+                        Tri tr = new Tri();
+                        double area1 = tr.Lenwei(l, w);
+                        Console.Write("三角形： 高 = {0}，底 = {1}，面積為 {2}\n",l,w,area1);
+                        break;
+
+                    case "2":
+                        Console.Write("請輸入 高  ");
+                        l = double.Parse(Console.ReadLine());
+                        Console.Write("請輸入 底  ");
+                        w = double.Parse(Console.ReadLine());
+                        Rect re = new Rect();
+                        double area2 = re.Lenwei(l, w);
+                        Console.Write("矩形： 高 = {0}，底 = {1}，面積為 {2}\n", l, w, area2);
+                        break;
+
+                    case "3":
+                        Console.Write("請輸入 高  ");
+                        l = double.Parse(Console.ReadLine());
+                        Console.Write("請輸入 底  ");
+                        w = double.Parse(Console.ReadLine());
+                        Dia di = new Dia();
+                        double area3 = di.Lenwei(l, w);
+                        Console.Write("菱形： 高 = {0}，底 = {1}，面積為 {2}\n", l, w, area3);
+                        break;
+
+                    case "4":
+                        Console.Write("請輸入 高  ");
+                        l = double.Parse(Console.ReadLine());
+                        Console.Write("請輸入 底  ");
+                        w = double.Parse(Console.ReadLine());
+                        Para pa = new Para();
+                        double area4 = pa.Lenwei(l, w);
+                        Console.Write("平行四邊形： 高 = {0}，底 = {1}，面積為 {2}\n", l, w, area4);
+                        break;
+                    default:
+                        Console.Write("輸入錯誤\n");
+                        break;
+
+
+                }
+
+            }
+        }
+    }
+
+    interface ILenwei
+    {
+        
+        double Lenwei(double a,double b);
+    }
+
+    class Tri: ILenwei
+    {
+        public double Lenwei(double len, double wei)
+        {
+            return 0.5* len* wei;
+        }
+    }
+
+    class Rect : ILenwei
+    {
+        public double Lenwei(double len, double wei)
+        {
+            return len * wei;
+        }
+    }
+
+    class Dia : ILenwei
+    {
+        public double Lenwei(double len, double wei)
+        {
+            return len * wei;
+        }
+    }
+
+    class Para : ILenwei
+    {
+        public double Lenwei(double len, double wei)
+        {
+            return len * wei;
+        }
+    }
+}
